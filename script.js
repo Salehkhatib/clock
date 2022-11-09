c36216da3a303cf9ff63efd67e8cc8dc765a8294
Salehkhatib <skhatib552@gmail.com>
2022-11-09 11:07:54 +0100

    Clock

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/////digital-clock
 function showTime(){
 
     'use strict';
@@ -29,3 +29,28 @@ window.onload = function (){
 
      setInterval(showTime, 500);
 };
+
+
+/////analog-clock
+
+const secondsHand = document.getElementById('seconds-hand')
+const minutesHand = document.getElementById('minutes-hand')
+const hoursHand = document.getElementById('hours-hand')
+
+function getTime(){
+    const now = new Date()
+   
+    const seconds = now.getSeconds()
+
+    const minutes = now.getMinutes()
+    
+    const hours = now.getHours()
+
+    const timeInterval = 6
+
+    secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg)'
+    minutesHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds /10) + 'deg)'
+    hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2 ) + 'deg)'
+}
+setInterval(getTime, 100)
+
